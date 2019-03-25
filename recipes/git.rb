dff549baf0e8f439b44473b1c181c1cb2dfaf437
Dax T. Games <dax.games@bedbath.com>
2019-03-25 15:39:25 -0400

    fixes

--- recipes/git.rb
+++ recipes/git.rb
@@ -1,9 +1,9 @@
 case node['platform_family']
 when 'windows'
-  git_client 'Git version 2.11.0' do
-    windows_package_version '2.11.0'
-    windows_package_url 'https://github.com/git-for-windows/git/releases/download/v2.11.0.windows.1/Git-2.11.0-64-bit.exe'
-    windows_package_checksum 'fd1937ea8468461d35d9cabfcdd2daa3a74509dc9213c43c2b9615e8f0b85086'
+  git_client 'Git version' do
+    windows_package_version '2.21.0'
+    windows_package_url 'https://github.com/git-for-windows/git/releases/download/v2.21.0.windows.1/Git-2.21.0-64-bit.exe'
+    windows_package_checksum 'c7792387ebd69b3e11b7cc7b92c743f75c180275fa0ce9c7f0c5c7e44e470f80'
   end
 
   chocolatey_package 'git-credential-winstore'
