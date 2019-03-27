92148ac45dea2eb3b55e85a042d8b4ac5d5c0a87
Dax T. Games <dax.games@bedbath.com>
2019-03-27 11:10:51 -0400

    fixes

--- recipes/htop.rb
+++ recipes/htop.rb
@@ -4,5 +4,23 @@ when 'mac_os_x'
 when 'debian'
   apt_package 'htop'
 when 'rhel'
+  p "Platform: #{node['platform']}"
+  p "Platform Version: #{node['platform_version']}"
+
+  remote_file '/tmp/epel.rpm' do
+    source 'https://dl.fedoraproject.org/pub/epel/6/x86_64/Packages/e/epel-release-6-8.noarch.rpm'
+    only_if {node['platform'] != 'fedora' && node['platform_version'] =~ /^6/}
+  end
+
+  remote_file '/tmp/epel.rpm' do
+    source 'https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm'
+    only_if {node['platform'] != 'fedora' && node['platform_version'] =~ /^7/}
+  end
+
+  yum_package 'epel-release' do
+    source '/tmp/epel.rpm'
+    only_if {File.exist('/tmp/epel.rpm')}
+  end
+
   yum_package 'htop'
 end
