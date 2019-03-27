e8793ef5e952e79144ef5d66dd7a712599eeb768
Dax T. Games <dax.games@bedbath.com>
2019-03-27 11:16:33 -0400

    fixes

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,19 +11,19 @@ elsif node['platform_family'] == 'rhel'
   p "Platform: #{node['platform']}"
   p "Platform Version: #{node['platform_version']}"
 
-  remote_file '/tmp/epel.rpm' do
+  remote_file '/tmp/epel-release.rpm' do
     source 'https://dl.fedoraproject.org/pub/epel/6/x86_64/Packages/e/epel-release-6-8.noarch.rpm'
     only_if {node['platform'] != 'fedora' && node['platform_version'] =~ /^6/}
   end
 
-  remote_file '/tmp/epel.rpm' do
+  remote_file '/tmp/epel-release.rpm' do
     source 'https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm'
     only_if {node['platform'] != 'fedora' && node['platform_version'] =~ /^7/}
   end
 
   yum_package 'epel-release' do
-    source '/tmp/epel.rpm'
-    only_if {File.exist('/tmp/epel.rpm')}
+    source '/tmp/epel-release.rpm'
+    only_if {File.exist?('/tmp/epel-release.rpm')}
   end
 
   # include_recipe "#{cookbook_name}::centos_rhel"
